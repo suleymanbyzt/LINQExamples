435f9354917c2655c611b48fa606e5a5076d382c
Süleyman Bayazit <suleymanbayazitt@gmail.com>
2022-01-18 22:00:32 +0300

    new files added

---
+++ OrderByDescending/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OrderByDescending
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Rank { get; set; }
+
+    }
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           List<Student> list = new List<Student>();
+            list.Add(new Student { Id = 1, Name = "Süleyman", Rank = 2, Age = 23 });
+            list.Add(new Student { Id = 2, Name = "Elif", Rank = 1, Age = 18 });
+            list.Add(new Student { Id = 3, Name = "Deniz", Rank = 1, Age = 46 });
+            list.Add(new Student { Id = 4, Name = "Mehmet", Rank = 2, Age = 52 });
+
+
+            var studentsOrderById = list.OrderByDescending(x => x.Id);
+            Console.WriteLine("Sorted Students");
+            foreach (var student in studentsOrderById)
+            {
+                Console.WriteLine(student.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Reverse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[] { 1, 2, 3 , 4, 5, 6, 7};
+
+            foreach (var item in x)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Reverse");
+
+            var reverse = x.Reverse();
+
+            foreach (var item2 in reverse)
+            {
+                Console.WriteLine(item2);
+            }
+        }
+    }
+}
