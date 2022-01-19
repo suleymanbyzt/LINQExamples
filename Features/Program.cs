75a385a3b1129fdbe43c8552b5f34c6a390d0cd0
Süleyman Bayazit <suleymanbayazitt@gmail.com>
2022-01-19 21:55:12 +0300

    New files added

---
+++ Features/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Features
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> employees = new List<Employee>()
+            {
+                new Employee { Id=1, Name="Süleyman"},
+                new Employee { Id=2, Name="Scott"},
+                new Employee{ Id=3, Name="Barış"},
+                new Employee { Id=4, Name="Derya"}
+            };
+
+            foreach (var employee in employees.Where(
+                                     e=>e.Name.StartsWith("B")))
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+        }
+    }
+}
---
+++ Path/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Path
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+            ShowLargeFilesWithLinq(path);
+
+            List<string> files = new List<string>();    
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            var query = new DirectoryInfo(path).GetFiles()
+                .OrderByDescending(f => f.Length)
+                .Take(10);
+
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item.Name,-20} : {item.Length , 10}");
+            }
+        }
+    }
+}
